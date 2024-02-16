c2c73824d8cf0f71775f45b1eed00a07446afe85
benny12332 <91334953+benny12332@users.noreply.github.com>
2024-02-16 15:12:20 +0800

    Update 00-create-user.sh

--- scripts/00-create-user.sh
+++ scripts/00-create-user.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
 
+if [ $# -ne 3 ]
+then
+    echo "Please input arguments! "
+    echo " 00-create-user.sh <htpasswd_file_name> <user> <password> "
+    exit
+fi
 # export htpasswd
-oc get secret htpasswd -ojsonpath={.data.htpasswd} -n openshift-config | base64 --decode > users.htpasswd
+oc get secret htpasswd -ojsonpath={.data.htpasswd} -n openshift-config | base64 --decode > $1
 # add user
-htpasswd -bB users.htpasswd benny benny
+htpasswd -bB users.htpasswd $2 $3
 # apply new htpasswd file
-oc create secret generic htpasswd --from-file=htpasswd=users.htpasswd --dry-run=client -o yaml -n openshift-config | oc replace -f -
+oc create secret generic htpasswd --from-file=htpasswd=$1 --dry-run=client -o yaml -n openshift-config | oc replace -f -
