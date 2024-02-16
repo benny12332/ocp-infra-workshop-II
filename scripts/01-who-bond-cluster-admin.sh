d00206698f89daf207da19b9ada0c669788c1063
benny12332 <91334953+benny12332@users.noreply.github.com>
2024-02-16 10:30:05 +0800

    Update 01-who-bond-cluster-admin.sh

--- scripts/01-who-bond-cluster-admin.sh
+++ scripts/01-who-bond-cluster-admin.sh
@@ -1,1 +1,2 @@
-oc get clusterrolebindings -o json | jq '.items[] | select(.metadata.name=="cluster-admins") | .subjects[].name'
+#oc get clusterrolebindings -o json | jq '.items[] | select(.metadata.name=="cluster-admins") | .subjects[].name'
+oc get clusterrolebindings -o json | jq '.items[] | select(.roleRef.name=="cluster-admin")' | jq '.subjects[0].name' 
