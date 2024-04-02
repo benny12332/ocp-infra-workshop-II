f99dbd58890598c3861cd094408e69b88da11796
benny12332 <91334953+benny12332@users.noreply.github.com>
2024-04-03 01:25:50 +0800

    Update request.sh

--- scripts/session/hpa/request.sh
+++ scripts/session/hpa/request.sh
@@ -1,3 +1,3 @@
 ROUTE=`oc get route route-pod-autoscale --template={{.spec.host}}`
 SVC=`oc get svc svc-pod-autoscale --template={{.spec.clusterIP}}`
-while true;do curl ($ROUTE or $SVC) ;done
+while true;do curl ${ROUTE}:8080 ;done
